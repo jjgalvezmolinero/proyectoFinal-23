73713102b35d6a110b04364473f541dcfffb04ae
Juan José Gálvez Molinero <juanjosegalvezmolinero@hotmail.com>
2023-02-05 20:29:56 +0100

    Se ha creado los archivos de la parte de la finca

---
+++ cooperativa/views/finca.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../classes/UserSession.php';
+require_once '../classes/Menu.php';
+require_once '../classes/Header.php';
+require_once '../classes/Footer.php';
+$UserSession = new UserSession();
+$Menu = new Menu();
+$Header = new Header();
+$Footer = new Footer();
+$Header->headerBasico();
+$Menu->menuBasico();
+
+$Footer->footer();
