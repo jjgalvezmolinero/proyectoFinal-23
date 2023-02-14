0556136f0d33c6ea3c99b33520da025ba0b15bd3
Juan José Gálvez Molinero <juanjosegalvezmolinero@hotmail.com>
2023-02-14 17:21:25 +0100

    Se ha añadido la página principal, donde se mostrarán todos los datos del usuario

---
+++ cooperativa/js/principal.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  // let shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+  // console.log(shopping);
+  // const values = [0.409, 0.15, 0.857, 0.0583, 0.183];
+  // const data  = {
+  //   labels: shopping,
+  //   values: values
+  // }
+  // drawBarChart(
+  //   $("#example"),data,
+  //   {
+  //     id: 2,
+  //     chartTitle: "Deposit interest rate in 2020",
+  //     yAxisTitle: "Deposit interest rate (%)",
+  //     xAxisTitle: "Country",
+  //   }
+  // );
+});
--- cooperativa/views/principal.php
+++ cooperativa/views/principal.php
@@ -5,17 +5,27 @@ require_once '../classes/Header.php';
 require_once '../classes/Footer.php';
 
 $js = array(
-  '../js/principal.js'
+  '../js/principal.js',
+  '../library/graph/draw-bar-chart.js'
+);
+
+$css = array(
+  '../library/graph/draw-bar-chart.css'
 );
 
 $UserSession = new UserSession();
 $Menu = new Menu();
 $Header = new Header();
-$Footer = new Footer($js);
+$Footer = new Footer($js,$css);
 $Header->headerBasico();
 $Menu->menuBasico();
 ?>
-
+<div class="container">
+  <div class="row mb-3">
+    <div class="col">.col</div>
+    <div class="col">.col</div>
+  </div>
+</div>
 <?php
 $Footer->footer();
 ?>
